854367437008c5f1b584029104c82f3b60f989e6
= <=dave.jodatek@gmail.com>
2022-07-18 16:37:07 +0100

    implementing templates with bootsrap

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import Category, Products
 # Create your views here.
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def all_products(request):
+    products = Products.objects.all()
+    return render(request, 'store/home.html', {'products': products})
